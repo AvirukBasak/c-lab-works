ac0e0c8cbdd2fdc4c443276f037137e6dfc85a0f
AvirukBasak <avirukbasak@gmail.com>
2022-04-02 21:24:37 +0530

    new repository

---
+++ grading_system.c
@@ -0,0 +1,37 @@
+/* Program for grading marks obtained by a student
+ * using conditional statements.
+ */
+
+# include <stdio.h>
+
+int main()
+{
+    int student_marks = 0;
+
+    printf ("Enter marks (out of 100): ");
+    scanf ("%d", &student_marks);
+
+    if (student_marks < 0) {
+        printf ("error: marks entered subceeds 0\n");
+        return 0;
+    } else if (student_marks > 100) {
+        printf ("error: marks entered exceeds 100\n");
+        return 0;
+    }
+
+    printf ("Grade: ");
+    if (student_marks <= 40) {
+        printf ("F\n");
+    } else if (student_marks > 40 && student_marks <= 55) {    // range of 15
+        printf ("E\n");
+    } else if (student_marks > 55 && student_marks <= 70) {    // range of 15
+        printf ("D\n");
+    } else if (student_marks > 70 && student_marks <= 85) {    // range of 15
+        printf ("C\n");
+    } else if (student_marks > 85 && student_marks <= 95) {    // range of 10
+        printf ("B\n");
+    } else if (student_marks > 95 && student_marks <= 100) {   // range of 5
+        printf ("A\n");
+    }
+    return 0;
+}
