3677182706663c74d5abba8eade402bef40fece2
Aviruk Basak <avirukbasak@gmail.com>
2022-08-19 09:39:28 +0530

    Update mycat.cpp

--- c++/mycat.cpp
+++ c++/mycat.cpp
@@ -4,13 +4,12 @@
 #define ERR_ARGS (1)
 #define VERSION  "0.0.1"
 
-bool printError()
+bool printHelp()
 {
     std::cout << "mycat: too few arguments\n";
     std::cout << "USAGE: mycat [files]\n";
     std::cout << "       mycat -n | --lineno [files]\n";
     std::cout << "       mycat -v | --version\n";
-    std::exit(ERR_ARGS);
     return true;
 }
 
@@ -18,11 +17,17 @@ int main(int argc, char **argv)
 {
     int i = 1, lineno = 1;
     bool showLinNum = false;
-    argc < 2 && printError();
-    if (!std::strcmp(argv[1], "-n") || !std::strcmp(argv[1], "--lineno")) {
+    bool cinStdin = false;
+    if (argc < 2)
+        cinStdin = false;
+    if (!std::strcmp(argv[1], "-h") || !std::strcmp(argv[1], "--help")) { 
+        printHelp();
+        exit(0);
+    } else if (!std::strcmp(argv[1], "-n") || !std::strcmp(argv[1], "--lineno")) {
         i = 2;
         showLinNum = true;
-        argc < 3 && printError();
+        argc < 3 && printHelp();
+        std::exit(ERR_ARGS);
     } else if (!std::strcmp(argv[1], "-v") || !std::strcmp(argv[1], "--version")) {
         std::cout << "MyCat Program v" VERSION "\n";
         std::cout << "Authors: Aviruk Basak\n";
@@ -30,13 +35,18 @@ int main(int argc, char **argv)
         exit(0);
     }
     for (; i < argc; i++) {
-        std::ifstream file(argv[i]);
+        std::ifstream file;
+        if (cinStdin)
+            file = std::cin;
+        else
+            file.open(argv[i]);
         std::string line = "";
         while (getline(file, line)) {
             showLinNum && std::cout << lineno++ << "\t";
             std::cout << line << "\n";
         }
-        file.close();
+        if (!cinStdin)
+            file.close();
     }
     return 0;
 }
