527cd4ee294f6ab0bd19646911b66684aae43830
OogleGlu <avirukbasak@gmail.com>
2022-09-08 04:22:08 +0530

    added sparse matrix implemenration in sll and arr

--- algorithms/sparse-matrix-to-tuple-array.c
+++ algorithms/sparse-matrix-to-tuple-array.c
@@ -8,7 +8,120 @@
 #include <stdlib.h>
 #include <stdbool.h>
 
+typedef struct Tuple {
+    int row;
+    int col;
+    int val;
+} Tuple;
+
+typedef struct TupleArray {
+    size_t length;
+    Tuple *arr;
+} *TupleArray;
+
+TupleArray new_tupleArr(size_t size);
+TupleArray matrix_toTuple3Arr(int rows, int cols, int matrix[rows][cols], size_t size);
+size_t matrix_countNonZero(int rows, int cols, int matrix[rows][cols]);
+void matrix_printArr(TupleArray arr);
+void matrix_freeArr(TupleArray *arr);
+
+TupleArray new_tupleArr(size_t size)
+{
+    TupleArray arr = malloc(sizeof(struct TupleArray));
+    if (!arr) abort();
+    arr->arr = malloc(size * sizeof(struct Tuple));
+    if (!arr->arr) abort();
+    arr->length = size;
+    return arr;
+}
+
+TupleArray matrix_toTuple3Arr(int rows, int cols, int matrix[rows][cols], size_t size)
+{
+    int i, j, k;
+    TupleArray arr = new_tupleArr(size);
+    for (i = 0, k = 0; i < rows; i++)
+        for (j = 0; j < cols; j++)
+            if (matrix[i][j] != 0) {
+                arr->arr[k].row = i;
+                arr->arr[k].col = j;
+                arr->arr[k].val = matrix[i][j];
+                k++;
+            }
+    return arr;
+}
+
+size_t matrix_countNonZero(int rows, int cols, int matrix[rows][cols])
+{
+    int nonZeroCount = 0;
+    int i, j;
+    for (i = 0; i < rows; i++)
+        for (j = 0; j < cols; j++)
+            if (matrix[i][j] != 0)
+                nonZeroCount++;
+    return nonZeroCount;
+}
+
+void matrix_printArr(TupleArray arr)
+{
+    int i;
+    if (!arr) abort();
+    printf("{\n");
+    for (i = 0; i < arr->length; i++)
+        printf("    (%d, %d): %d\n", arr->arr[i].row, arr->arr[i].col, arr->arr[i].val);
+    printf("}\n");
+}
+
+void matrix_freeArr(TupleArray *arr)
+{
+    if (!*arr || !(*arr)->arr) return;
+    free((*arr)->arr);
+    free(*arr);
+    *arr = NULL;
+}
+
 int main()
 {
+    int i, j, rows, cols;
+    printf("enter row and col size: ");
+    scanf("%d", &rows);
+    scanf("%d", &cols);
+    // C-99 variable array definition syntax: stack allocated arrays
+    int matrix[rows][cols];
+    printf("\ninput data in matrix:\n");
+    for (i = 0; i < rows; i++) {
+        printf("row %d: ", i);
+        for (j = 0; j < cols; j++)
+            scanf("%d", &matrix[i][j]);
+    }
+    size_t size = matrix_countNonZero(rows, cols, matrix);
+    if (size > rows * cols * 1 / 3) {
+        printf("\nerror: input isn't a sparse matrix\n");
+        return 0;
+    }
+    TupleArray arr = matrix_toTuple3Arr(rows, cols, matrix, size);
+    printf("\nresult = ");
+    matrix_printArr(arr);
+    matrix_freeArr(&arr);
     return 0;
 }
+
+/* OUTPUT:
+
+enter row and col size: 4 5
+
+input data in matrix:
+row 0: -1 0 0 0 2
+row 1: 4 0 5 0 0
+row 2: 0 0 0 0 1
+row 3: 7 0 0 0 0 
+
+result = {
+    (0, 0): -1
+    (0, 4): 2
+    (1, 0): 4
+    (1, 2): 5
+    (2, 4): 1
+    (3, 0): 7
+}
+
+*/
--- algorithms/sparse-matrix-to-tuple-sll.c
+++ algorithms/sparse-matrix-to-tuple-sll.c
@@ -8,7 +8,122 @@
 #include <stdlib.h>
 #include <stdbool.h>
 
+typedef struct TupleNode {
+    int row;
+    int col;
+    int val;
+    struct TupleNode *next;
+} *TupleNode;
+
+TupleNode new_tuplenode(int row, int col, int val);
+TupleNode matrix_toTuple3Sll(int rows, int cols, int matrix[rows][cols]);
+bool matrix_isSparse(int rows, int cols, int matrix[rows][cols]);
+void matrix_printSll(TupleNode head);
+void matrix_freeSll(TupleNode *head);
+
+TupleNode new_tuplenode(int row, int col, int val)
+{
+    TupleNode node = malloc(sizeof(struct TupleNode));
+    if (!node) abort();
+    node->row = row;
+    node->col = col;
+    node->val = val;
+    node->next = NULL;
+    return node;
+}
+
+TupleNode matrix_toTuple3Sll(int rows, int cols, int matrix[rows][cols])
+{
+    int i, j;
+    TupleNode head = new_tuplenode(0,0,0);
+    TupleNode p = head;
+    for (i = 0; i < rows; i++)
+        for (j = 0; j < cols; j++)
+            if (matrix[i][j] != 0) {
+                p->next = new_tuplenode(i, j, matrix[i][j]);
+                p = p->next;
+            }
+    return head;
+}
+
+bool matrix_isSparse(int rows, int cols, int matrix[rows][cols])
+{
+    int elCount = rows * cols;
+    int zeroCount = 0;
+    int i, j;
+    for (i = 0; i < rows; i++)
+        for (j = 0; j < cols; j++)
+            if (matrix[i][j] == 0)
+                zeroCount++;
+    return zeroCount > elCount * 2 / 3;
+}
+
+void matrix_printSll(TupleNode head)
+{
+    if (!head) abort();
+    TupleNode p = head->next;
+    printf("{\n");
+    while (p != NULL) {
+        printf("    (%d, %d): %d\n", p->row, p->col, p->val);
+        p = p->next;
+    }
+    printf("}\n");
+}
+
+void matrix_freeSll(TupleNode *head)
+{
+    if (!*head) return;
+    TupleNode p = *head;
+    while (p != NULL) {
+        TupleNode tmp = p->next;
+        free(p);
+        p = tmp;
+    }
+    *head = NULL;
+}
+
 int main()
 {
+    int i, j, rows, cols;
+    printf("enter row and col size: ");
+    scanf("%d", &rows);
+    scanf("%d", &cols);
+    // C-99 variable array definition syntax: stack allocated arrays
+    int matrix[rows][cols];
+    printf("\ninput data in matrix:\n");
+    for (i = 0; i < rows; i++) {
+        printf("row %d: ", i);
+        for (j = 0; j < cols; j++)
+            scanf("%d", &matrix[i][j]);
+    }
+    if (!matrix_isSparse(rows, cols, matrix)) {
+        printf("\nerror: input isn't a sparse matrix\n");
+        return 0;
+    }
+    TupleNode head = matrix_toTuple3Sll(rows, cols, matrix);
+    printf("\nresult = ");
+    matrix_printSll(head);
+    matrix_freeSll(&head);
     return 0;
 }
+
+/* OUTPUT:
+
+enter row and col size: 4 5
+
+input data in matrix:
+row 0: -1 0 0 0 2
+row 1: 4 0 5 0 0
+row 2: 0 0 0 0 1
+row 3: 7 0 0 0 0
+
+result = {
+    (0, 0): -1
+    (0, 4): 2
+    (1, 0): 4
+    (1, 2): 5
+    (2, 4): 1
+    (3, 0): 7
+}
+
+*/
